565318db97eec2f4098e03b8a443efc8d69146d7
Jacobokia <cobs399@gmail.com>
2023-08-14 21:16:26 +0300

    changes the adapter from Base adapter to Array adapter

--- app/src/main/java/com/cobs/todoapp/MainActivity.kt
+++ app/src/main/java/com/cobs/todoapp/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.cobs.todoapp
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.ListAdapter
+import android.widget.TextView
 import com.cobs.todoapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -14,22 +18,35 @@ class MainActivity : AppCompatActivity() {
 
         val myList = binding.myList
 
-        val list = ArrayList<Country>()
-        list.add(Country("Uganda", "Kampala"))
-        list.add(Country("Kenya", "Nairobi"))
-        list.add(Country("Rwanda", "Kigali"))
-        list.add(Country("DRC", "Kinshasa"))
-        list.add(Country("Tanzania", "Dodoma"))
-        list.add(Country("USA", "Washington DC"))
-        list.add(Country("Burundi", "Bujumbura"))
-        list.add(Country("UK", "London"))
-        list.add(Country("Canada", "Montreal"))
+//        val list = ArrayList<Country>()
+//        list.add(Country("Uganda", "Kampala"))
+//        list.add(Country("Kenya", "Nairobi"))
+//        list.add(Country("Rwanda", "Kigali"))
+//        list.add(Country("DRC", "Kinshasa"))
+//        list.add(Country("Tanzania", "Dodoma"))
+//        list.add(Country("USA", "Washington DC"))
+//        list.add(Country("Burundi", "Bujumbura"))
+//        list.add(Country("UK", "London"))
+//        list.add(Country("Canada", "Montreal"))
 
-        //val countryList = listOf("", "Kenya", "South Sudan", "Rwanda", "DRC", "Tanzania",
-       // "USA","Burundi","UK", "Canada")
+        val countryList = listOf("", "Kenya", "South Sudan", "Rwanda", "DRC", "Tanzania",
+      "USA","Burundi","UK", "Canada")
+        val cityList = listOf("Nairobi", "Juba", "Kigali", "Kinshasa", "Dodoma", "Washington",
+        "Bujumbura", "London", "Montreal")
+     val arrayAdapter = NewAdapter(this, countryList, cityList)
+       myList.adapter = arrayAdapter
 
-        val arrayAdapter = CountryAdapter(this, list)
-        myList.adapter = arrayAdapter
+    }
 
+    class NewAdapter(context: Context,val countryList: List<String>,val cityList: List<String>):ArrayAdapter<String>(context,R.layout.list_item_layout, R.id.country_name, countryList){
+        override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+            val view = super.getView(position, convertView, parent)
+
+            val countryName = view.findViewById<TextView>(R.id.country_name)
+            val cityName = view.findViewById<TextView>(R.id.city_name)
+            countryName.text = countryList[position]
+            cityName.text = cityList[position]
+            return view
+        }
     }
 }
